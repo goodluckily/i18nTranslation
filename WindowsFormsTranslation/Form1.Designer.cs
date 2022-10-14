c09ef1d7be804133e23869e9b237a51985524de2
陈阳 <cyrus@nep-logistics.com>
2022-10-14 10:24:13 +0800

    i18nTranslation翻译小工具第一版

---
+++ WindowsFormsTranslation/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace WindowsFormsTranslation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Green;
+            this.button1.Location = new System.Drawing.Point(303, 155);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "翻译";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.button2.Location = new System.Drawing.Point(82, 155);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(82, 39);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "清除";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("宋体", 13F);
+            this.textBox1.Location = new System.Drawing.Point(82, 25);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(316, 30);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label
+            // 
+            this.label.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label.Location = new System.Drawing.Point(27, 30);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(49, 21);
+            this.label.TabIndex = 3;
+            this.label.Text = "变量:";
+            this.label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label.Click += new System.EventHandler(this.变量名称_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("宋体", 13F);
+            this.textBox2.Location = new System.Drawing.Point(82, 69);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(316, 70);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("宋体", 13F);
+            this.richTextBox1.Location = new System.Drawing.Point(42, 200);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(420, 252);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label2.Location = new System.Drawing.Point(27, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 21);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "描述:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.Image = global::WindowsFormsTranslation.Properties.Resources.mi;
+            this.label1.Location = new System.Drawing.Point(132, 464);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 30);
+            this.label1.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(193, 474);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(179, 12);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "最终解释权归陈阳(Cyrus)所有。";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(482, 505);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "i18n小工具";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ WindowsFormsTranslation/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Web;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using Microsoft.International.Converters.TraditionalChineseToSimplifiedConverter;
+using Newtonsoft.Json;
+
+namespace WindowsFormsTranslation
+{
+    public partial class Form1 : Form
+    {
+        //APP ID 
+        public static string appId = "20210420000791175";
+        //密钥
+        public static string secretKey = "xhRL6tzMl5acjsZHaapC";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //结果
+            var nameText = textBox1.Text?.Trim();
+            var descText = textBox2.Text?.Trim();
+
+            var unescapeRetString = GetUnescapeRetString(descText, "zh", "en");
+            var amode = JsonConvert.DeserializeObject<TranslationResult>(unescapeRetString);
+            var traditionalStr = ChineseConverter.Convert(descText, ChineseConversionDirection.SimplifiedToTraditional);
+            //拼接数据
+            var strBudder = new StringBuilder();
+            
+            //strBudder.AppendLine($"{Environment.NewLine}");
+            
+            strBudder.AppendLine($"\"{nameText}\":\"{descText}\",");
+            strBudder.AppendLine($"\"{nameText}\":\"{traditionalStr}\",");
+            strBudder.AppendLine($"\"{nameText}\":\"{amode.trans_result.FirstOrDefault()?.dst}\",");
+            
+            //strBudder.AppendLine($"{Environment.NewLine}");
+
+            SetRichTextBox1(strBudder.ToString(), color: Color.Black);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            richTextBox1.Clear();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 变量名称_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// 翻译 中文(zh) 英语(en) 繁体中文(cht)
+        /// </summary>
+        /// <param name="text">原文</param>
+        /// <param name="from">源语言</param>
+        /// <param name="to">目标语言</param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        public string GetUnescapeRetString(string text, string from, string to)
+        {
+            try
+            {
+                Random rd = new Random();
+                string salt = rd.Next(100000).ToString();
+
+                string sign = EncryptString(appId + text + salt + secretKey);
+                string url = "http://api.fanyi.baidu.com/api/trans/vip/translate?";
+                url += "q=" + HttpUtility.UrlEncode(text);
+                url += "&from=" + from;
+                url += "&to=" + to;
+                url += "&appid=" + appId;
+                url += "&salt=" + salt;
+                url += "&sign=" + sign;
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                request.Method = "GET";
+                request.ContentType = "text/html";
+                request.UserAgent = null;
+                request.Timeout = 6000;
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                Stream myResponseStream = response.GetResponseStream();
+                StreamReader myStreamReader = new StreamReader(myResponseStream);
+                string retString = myStreamReader.ReadToEnd();
+                var UnescapeRetString = Regex.Unescape(retString);
+                myStreamReader.Close();
+                myResponseStream.Close();
+                return UnescapeRetString;
+            }
+            catch (Exception ex)
+            {
+                SetRichTextBox1(ex.Message, color: Color.Red);
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public void SetRichTextBox1(string str, Color color = default(Color))
+        {
+            richTextBox1.Clear();  // 清除文本内容
+
+            richTextBox1.BulletIndent = 30;//指定文本距离控件最左边缩进30个像素
+            richTextBox1.SelectionFont = new Font("Arial", 13);//设置当前插入文本的16号字体
+            richTextBox1.SelectionColor = color;  // 设定插入文本的颜色
+            richTextBox1.SelectedText = str;  // 插入的文本
+        }
+
+        // 计算MD5值
+        public static string EncryptString(string str)
+        {
+            MD5 md5 = MD5.Create();
+            // 将字符串转换成字节数组
+            byte[] byteOld = Encoding.UTF8.GetBytes(str);
+            // 调用加密方法
+            byte[] byteNew = md5.ComputeHash(byteOld);
+            // 将加密结果转换为字符串
+            StringBuilder sb = new StringBuilder();
+            foreach (byte b in byteNew)
+            {
+                // 将字节转换成16进制表示的字符串，
+                sb.Append(b.ToString("x2"));
+            }
+            // 返回加密的字符串
+            return sb.ToString();
+        }
+
+
+        /// <summary>
+        /// 接收百度翻译API结果的实体类
+        /// </summary>
+
+        public class TranslationResult
+        {
+            public string from { get; set; }
+            public string to { get; set; }
+            public Trans_Result[] trans_result { get; set; }
+        }
+
+        public class Trans_Result
+        {
+            public string src { get; set; }
+            public string dst { get; set; }
+        }
+
+        public class TranslationErrorResult
+        {
+            public string error_code { get; set; }
+            public string error_msg { get; set; }
+        }
+
+        
+    }
+}
